eb38aed5b67278af3430d4c22aff8ee1fdcfbe79
Samuel Loubiat <sloubiat@z4r10p3.42lyon.fr>
2026-04-18 18:26:26 +0200

    add utils.c

--- coders/utils.c
+++ coders/utils.c
@@ -1,3 +1,53 @@
-//
-// Created by sloubiat on 4/18/26.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   utils.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sloubiat <sloubiat@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/18 02:29:01 by sloubiat          #+#    #+#             */
+/*   Updated: 2026/04/18 02:55:58 by sloubiat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header/codexion.h"
+
+int	ft_get_time(void)
+{
+	struct timespec	ts;
+
+	clock_gettime(CLOCK_MONOTONIC, &ts);
+	return (ts.tv_sec * 1000LL + ts.tv_nsec / 1000000LL);
+}
+
+int	has_burned(t_arg *arg)
+{
+	pthread_mutex_lock(&arg->config->mutex_burn);
+	if (arg->config->burned)
+	{
+		pthread_mutex_unlock(&arg->config->mutex_burn);
+		free(arg);
+		return (1);
+	}
+	pthread_mutex_unlock(&arg->config->mutex_burn);
+	return (0);
+}
+
+int	has_finish(t_arg *arg, t_coders *coder)
+{
+	pthread_mutex_lock(&coder->coder_mutex);
+	if (arg->config->number_compile == coder->number_compile)
+	{
+		pthread_mutex_unlock(&coder->coder_mutex);
+		return (1);
+	}
+	pthread_mutex_unlock(&coder->coder_mutex);
+	return (0);
+}
+
+void	ft_put_str(char *str, int coder_id, int time, t_arg *arg)
+{
+	pthread_mutex_lock(&arg->config->mutex_console);
+	printf(str, time, coder_id);
+	pthread_mutex_unlock(&arg->config->mutex_console);
+}
