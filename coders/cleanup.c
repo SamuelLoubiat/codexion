f6ef7ffacaecc2a7c128f7f9486ccdee1f0fd260
Samuel Loubiat <sloubiat@z4r10p3.42lyon.fr>
2026-04-18 17:52:05 +0200

    add function to cleaning

--- coders/cleanup.c
+++ coders/cleanup.c
@@ -1,3 +1,42 @@
-//
-// Created by sloubiat on 4/18/26.
-//
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   cleanup.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: sloubiat <sloubiat@student.42lyon.fr>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/04/18 02:25:01 by sloubiat          #+#    #+#             */
+/*   Updated: 2026/04/18 02:28:45 by sloubiat         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "header/codexion.h"
+
+void	free_coders(t_coders *coders)
+{
+	t_coders	*temp;
+
+	coders->prev->next = NULL;
+	while (coders)
+	{
+		temp = coders->next;
+		pthread_mutex_destroy(&coders->coder_mutex);
+		free(coders);
+		coders = temp;
+	}
+}
+
+void	free_dongles(t_dongle *dongles)
+{
+	t_dongle	*temp;
+
+	dongles->prev->next = NULL;
+	while (dongles)
+	{
+		temp = dongles->next;
+		pthread_mutex_destroy(&dongles->mutex);
+		pthread_mutex_destroy(&dongles->queue_mutex);
+		free(dongles);
+		dongles = temp;
+	}
+}
